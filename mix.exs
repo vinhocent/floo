4e23064e7458262c4f5821f076b7fc7b77c552cb
Tri Ho <vinhocent@gmail.com>
2024-04-11 22:08:02 -0400

    added elixir app boiler plate

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule Elixirbot.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :elixirbot,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Elixirbot.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  def deps do
+    [{:nostrum, "~> 0.8"},
+    {:absinthe, "~> 1.6"},
+    {:absinthe_plug, "~> 1.5"}
+    ]
+
+  end
+end
